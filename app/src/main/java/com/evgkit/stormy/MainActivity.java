8bb5dd4ab97552dcf114c96cc9360b765bafb9ec
evgkit <kitaev.ea@gmail.com>
2018-05-13 17:14:44 +0300

    Add API call

--- app/src/main/java/com/evgkit/stormy/MainActivity.java
+++ app/src/main/java/com/evgkit/stormy/MainActivity.java
@@ -2,12 +2,53 @@ package com.evgkit.stormy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.Locale;
+
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    private final OkHttpClient client = new OkHttpClient();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "111b4406c7757474f9e4ba2bee689f93";
+        Double latitude = 37.8267;
+        Double longitude = -122.4233;
+
+        String forecastURL = String.format(
+                Locale.US,
+                "https://api.darksky.net/forecast/%s/%f,%f",
+                apiKey, latitude, longitude
+        );
+
+        Request request = new Request.Builder()
+                .url(forecastURL)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+
+            Headers responseHeaders = response.headers();
+            for (int i = 0; i < responseHeaders.size(); i++) {
+                System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+            }
+
+            System.out.println(response.body().string());
+        } catch (IOException e) {
+            Log.e(TAG, "IO Exception " + e);
+        }
     }
 }
